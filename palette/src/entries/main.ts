f32c9bf86a63ef9d1860755296229b04d46b0896
Natalie masse hooper <nataliemassehooper@gmail.com>
2025-03-14 11:11:29 +0000

    Fixed plugin name

--- palette/src/entries/main.ts
+++ palette/src/entries/main.ts
@@ -1,4 +1,4 @@
 import { loadPlugin } from '@/lib/loadPlugin'
 import PalettePlugin from '@/components/PalettePlugin.vue'
 
-loadPlugin('palette-with-colour-names', {}, PalettePlugin)
+loadPlugin('palette-with-colour-name', {}, PalettePlugin)
